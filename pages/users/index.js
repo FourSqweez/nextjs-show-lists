da3cc6e5bd06480a4ab0f123b6be7f764ca95354
FourSqweez <Kjeerasak@kkumail.com>
2021-02-16 13:13:17 +0700

    Added : getStaticProbs, Dynamic route

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,7 +1,26 @@
-const Users = () => {
+import style from '../../styles/Users.module.scss'
+import Link from 'next/link'
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+
+  return {
+    props: {
+      users: data,
+    },
+  }
+}
+const Users = ({ users }) => {
   return (
     <div>
       <h1>All users</h1>
+      {users.map((user) => (
+        <Link key={user.id} href={`/users/${user.id}`}>
+          <a className={style.single}>
+            <h3>{user.name}</h3>
+          </a>
+        </Link>
+      ))}
     </div>
   )
 }
